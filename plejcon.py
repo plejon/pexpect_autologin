2bc60da7f3dab0433694d610df0e3a97ff0aeeff
plejon <per@lejon.org>
2016-11-26 13:22:32 +0100

    Adding autologin script
    Adding autologin script

---
+++ plejcon.py
@@ -0,0 +1,94 @@
+#!usr/bin/python
+try:
+    import pexpect
+except:
+    print('module "pexpect" not found.\n"apt-get install python-pexpect"\nor\n "pip install pexpect"\nfor install\nscript does not work on windows :(')
+
+import socket, sys, os, logging, time
+
+def TryDiz(host):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        log.debug(' trying ssh socket')
+        testssh = sock.connect_ex((host, 22))
+        log.debug(' trying telnet socket')
+        testtel = sock.connect_ex((host, 23))
+        if testssh == 0:
+            log.debug(' SSH open @ %s' % host)
+            return True
+        elif testtel == 0:
+            log.debug(' Telnet open @ %s' % host)
+            return False
+        else:
+            log.warning(' Telnet/SSH not open on "%s"' % host)
+            sys.exit()
+    except:
+        sys.exit()
+
+def main():
+    if os.path.isfile('creds.txt') == True:
+        username, password = open('creds.txt', 'r').read().split(':')
+    else:
+        with open('creds.txt', 'w') as x:
+            print('Cannot find file with creds, enter creds to make on.')
+            username = raw_input('username: ')
+            password = raw_input('password: ')
+            x.write('%s:%s' % (username, password))
+
+    host = sys.argv[1]
+    x = TryDiz(host)
+    if x == True:
+        log.info(' Spawning SSH @ "%s"' % host)
+        ssh = pexpect.spawn('ssh "%s"' % host)
+        time.sleep(1)
+        x = ssh.expect(['continue connecting','assword',pexpect.EOF,pexpect.TIMEOUT],1)
+        if x == 0:
+            log.debug(' Auto adding SSH key for "%s"' % host)
+            ssh.sendline('yes')
+            x = ssh.expect(['continue connecting','assword',pexpect.EOF])
+        if x == 1:
+            log.debug(' Sending password "%s"' % host)
+            ssh.sendline(password)
+        elif x == 2:
+            log.warning('\n\nSomething did not work, plz interact.\n\n')
+            print ssh.before
+            sys.exit()
+        elif x == 3:
+            pass
+
+    elif x == False:
+        log.info(' Spawning Telnet @ "%s"' % host)
+        tel = pexpect.spawn('telnet "%s"' % host)
+        time.sleep(1)
+        x = tel.expect(['(?i)username', '(?i)login'])
+        if x == 1 or 2:
+            log.debug(' Sending username "%s"' % host)
+            tel.sendline(username)
+        tel.expect('(?i)password')
+        log.debug(' Sending password "%s"' % host)
+        tel.sendline(password)
+        
+    else:
+        log.warning(' Telnet/SSH could not connect to "%s"' % host)
+        sys.exit()
+
+    try:
+        if 'ssh' or 'tel' in locals():
+            log.debug(' Trying to enable user interaction "%s"' % host)
+            ssh.interact()
+            sys.exit()
+
+        elif 'tel' in locals():
+            log.debug(' Trying to enable user interaction "%s"' % host)
+            tel.interact()
+            sys.exit()
+
+    except Exception, e:
+        log.error('{} "{}"'.format(str(e), str(host)))
+        sys.exit()
+
+if __name__ == '__main__':
+    log = logging.getLogger('plejCON')
+    logging.basicConfig(level=10)
+    main()
